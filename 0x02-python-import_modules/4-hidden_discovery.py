b6d09f8e58ff4997dc60409465274d57af8ea4ac
Doutimi <isenahdoutimiebi@gmail.com>
2023-07-16 14:55:23 -0700

    task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    dnames = dir(hidden_4)
+    dnames = dir (hidden_4)
     for name in dnames:
         if name[:2] != "__":
             print(name)
