ae08238e110a3ea373dee0746867034695341b8c
Doutimi <isenahdoutimiebi@gmail.com>
2023-08-20 09:29:13 -0700

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A function that returns an object (Python data structure)
+represented by a JSON string:"""
+import json
+
+
+def from_json_string(my_str):
+    """"
+    Returns the object representation of  JSON string.
+    Arg:
+    my_str: object
+    """
+    return json.loads(my_str)
