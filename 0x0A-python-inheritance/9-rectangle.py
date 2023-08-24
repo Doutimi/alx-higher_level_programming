0a18f4daee22b009ffa5e6aec90e19cd6890b0ce
Doutimi <isenahdoutimiebi@gmail.com>
2023-08-24 10:19:25 -0700

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a Rectangle object"""
+
+    def __init__(self, width, height):
+        """Initializes a Rectangle object"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Returns str() representation of a Rectangle."""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+    def area(self):
+        """returns the area of rectangle"""
+        return self.__height * self.__width
