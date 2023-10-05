c24f22221197f77ce8f5040e08736478dade1765
Doutimi <isenahdoutimiebi@gmail.com>
2023-10-05 05:35:35 -0700

    extra

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,3 +3,10 @@ SELECT DISTINCT name
   FROM tv_genres AS genres
 	 INNER JOIN tv_show_genres AS show_genres
 	     ON genres.id = show_genres.genre_id
+	INNER JOIN tv_shows AS shows ON shows.id = show_genres.show_id
+WHERE genres.name NOT IN (
+	SELECT DISTINCT name FROM tv_genres AS genres
+	INNER JOIN tv_show_genres AS show_genres ON show_genres.genre_id = genres.id
+	INNER JOIN tv_shows AS shows ON shows.id = show_genres.show_id
+	WHERE title = 'Dexter')
+ORDER BY name;
