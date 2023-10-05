96328ece90170582dfe5462dbe8157b6c98e7773
Doutimi <isenahdoutimiebi@gmail.com>
2023-10-05 05:43:59 -0700

    extra

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,10 @@
 -- lists all shows without the genre `Comedy` in the database `hbtn_0d_tvshows`
-SELECT DISTINCT title
-  FROM tv_shows AS shows
-	 LEFT JOIN tv_show_genres AS show_genres
-	     ON show_genres.show_id = shows.id
-	ORDER BY title;
+SELECT DISTINCT title FROM tv_genres AS g
+RIGHT JOIN tv_show_genres AS tv_g ON tv_g.genre_id = g.id 
+RIGHT JOIN tv_shows AS show ON show.id = tv_g.show_id
+WHERE title NOT IN (
+		SELECT title FROM tv_genres AS g
+		INNER JOIN tv_show_genres AS tv_g ON tv_g.genre_id = g.id
+		INNER JOIN tv_shows AS show ON show.id = tv_g.show_id
+		WHERE name = 'Comedy')
+ORDER BY title;
