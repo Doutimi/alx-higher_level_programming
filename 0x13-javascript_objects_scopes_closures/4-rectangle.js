ca92f4dce7afa95750f9c5298b94c0c1009f63a6
Doutimi <isenahdoutimiebi@gmail.com>
2024-03-12 08:59:40 -0700

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let h = 0; h < this.height; h++) {
-        let line = '';
-        for (let w = 0; w < this.width; w++) {
-          line += 'X';
-        }
-        console.log(line);
-      }
-    }
-
-    rotate () {
-        let new_width = this.height;
-        let new_height = this.width;
-        this.width = new_width;
-        this.height = new_height;
-    }
-
-    double () {
-        this.width*=2;
-        this.height*=2;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
+
+  print () {
+    for (let h = 0; h < this.height; h++) {
+      let line = '';
+      for (let w = 0; w < this.width; w++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const nWidth = this.height;
+    const nHeight = this.width;
+    this.width = nWidth;
+    this.height = nHeight;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
