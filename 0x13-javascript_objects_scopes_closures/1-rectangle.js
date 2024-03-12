e3aa0b79388acfdefcc69ae006ead06191e2e83a
Doutimi <isenahdoutimiebi@gmail.com>
2024-03-12 07:39:29 -0700

    fix task 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
-    }
-	width;
-	height;
-  };
-  
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
