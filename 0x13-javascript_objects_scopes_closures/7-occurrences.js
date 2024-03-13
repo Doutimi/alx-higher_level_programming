78b52af9a627b703031531333c1f3abe6e4e26f1
Doutimi <isenahdoutimiebi@gmail.com>
2024-03-13 08:12:15 -0700

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
-/**
- * 
- * @param {string[]} list 
- * @param {any} searchElement 
- */
 exports.nbOccurences = function (list, searchElement) {
-    var count = 0;
-    for (let i = 0; i<list.length; i++) {
-        if (list[i] == searchElement)
-            count++;
-    }
-    return count;
-    
-}
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) { count++; }
+  }
+  return count;
+};
