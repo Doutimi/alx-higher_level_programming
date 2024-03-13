d2f8272e7c50d7f40b256e6cb0ae3a74e0868525
Doutimi <isenahdoutimiebi@gmail.com>
2024-03-13 16:37:52 +0100

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    let reversedlist = [];
+    for (let i = list.length-1; i>=0; i--) {
+        reversedlist = reversedlist.concat (list[i]);
+    }
+    return reversedlist;
+}
+
