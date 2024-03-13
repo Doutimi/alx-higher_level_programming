292c8c732379be6cc098cd98ed895057aaf6d029
Doutimi <isenahdoutimiebi@gmail.com>
2024-03-13 07:46:52 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 class Square extends require('./5-square') {
-    constructor (size) {
-      super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint(c) {
-      if (!c) this.print();
-      else {
-        for (let h = 0; h < this.height; h++) {
-          let line = "";
-          for (let w = 0; w < this.width; w++) {
-            line += c;
-          }
-          console.log(line);
+  charPrint (c) {
+    if (!c) this.print();
+    else {
+      for (let h = 0; h < this.height; h++) {
+        let line = '';
+        for (let w = 0; w < this.width; w++) {
+          line += c;
         }
+        console.log(line);
       }
     }
   }
-module.exports = Square
+}
+module.exports = Square;
