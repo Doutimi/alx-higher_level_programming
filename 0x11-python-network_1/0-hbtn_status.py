177e068dacf5e6173a8026471957b3d29d09a1b2
Doutimi <isenahdoutimiebi@gmail.com>
2024-04-10 16:32:16 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Write a Python script that fetches https://intranet.hbtn.io/status"""
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == '__main__':
 
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
         print('Body response:')
